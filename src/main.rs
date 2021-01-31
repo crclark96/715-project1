ab873831ec227f3cef16818820998a0331cc7378
Collin Reilly Clark <crclark96@gmail.com>
2021-01-31 15:24:42 -0500

    add blink function, some morse code sequences

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ use arduino_uno::prelude::*;
 use arduino_uno::hal::port::portb::PB5;
 use arduino_uno::hal::port::mode::Output;
 
+const DOT_MS: u16 = 200;
+const DASH_MS: u16 = DOT_MS * 3;
+const CODES: [&str; 36] = [
+                            "01","1000","1010","100","0","0010","110","0000",
+                            "00","0111","101","0100","11","10","111","0110",
+                            "1101","010","000","1","001","0001","011","1001",
+                            "1011","1100","11111","01111","00111","00011",
+                            "00001","00000","10000","11000","11100","11110"
+                           ];
+
 #[arduino_uno::entry]
 fn main() -> ! {
     let peripherals = arduino_uno::Peripherals::take().unwrap();
@@ -27,16 +37,25 @@ fn main() -> ! {
     ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").void_unwrap();
 
     loop {
-        stutter_blink(&mut led, 25);
-        let b = nb::block!(serial.read()).void_unwrap();
+        // let b = nb::block!(serial.read()).void_unwrap();
 
-        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).void_unwrap();
+        // ufmt::uwriteln!(&mut serial, "Got {}!\r", b).void_unwrap();
+        blink(&mut led, "0110");
     }
 }
 
-fn stutter_blink(led: &mut PB5<Output>, times: usize) {
-    for i in 0..times {
-        led.toggle().void_unwrap();
-        arduino_uno::delay_ms((i*10) as u16);
+fn blink(led: &mut PB5<Output>, code: &str) {
+    led.set_low().void_unwrap();
+    for i in code.chars() {
+        led.set_high().void_unwrap();
+        match i {
+            '0' => arduino_uno::delay_ms(DOT_MS),
+            '1' => arduino_uno::delay_ms(DASH_MS),
+            _ => (),
+        }
+        led.set_low().void_unwrap();
+        arduino_uno::delay_ms(DOT_MS);
     }
+    arduino_uno::delay_ms(DASH_MS);
 }
+
